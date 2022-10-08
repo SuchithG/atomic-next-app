036e34950fa3f5a879bcf09c6e55df64b13be230
SuchithG <suchithgowda1995@gmail.com>
2022-10-09 00:19:01 +0530

    initial commit

--- Atoms/Title.js
+++ Atoms/Title.js
@@ -3,7 +3,7 @@ import styles from "../styles/title.module.scss"
 export default function Title() {
     return (
         <h1 className={styles.blogTile}>
-            How to Monitor Your Frontend and Perform Product Analysis with LogRocket
+            My Test Projects
         </h1>
     );
 }
